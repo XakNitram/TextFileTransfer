15ce8ef24ac611c6aaeb844cc865d691c16f4cbf
Tate Mioton <tatemioton@outlook.com>
2019-02-18 23:02:49 -0600

    Update Comments; Add Timeout; Fix Debug Messages; Add UDP Port Connection Test
    Comments have been expanded.
    
    A timeout has been added to both the client and server UDP connections to quit the program and inform the user to retry the transfer.
    
    Some messages were sending at the level of info when they were meant to be debug messages. This has been fixed. New debug messages have been added.
    
    A test has been added to test whether the random port selected has already been taken.

--- client.py
+++ client.py
@@ -12,9 +12,10 @@ def tostr(x):
         return str(x)
 
 
-# logging setup
+# logging setup. I got bored.
 root_logger = logging.getLogger("client")
-root_logger.setLevel(logging.DEBUG)
+# root_logger.setLevel(logging.DEBUG)
+root_logger.setLevel(logging.INFO)
 
 # handler setup
 # formatters   - https://docs.python.org/3.7/library/logging.html#formatter-objects
@@ -23,7 +24,7 @@ handlr = logging.StreamHandler(stderr)
 fmtr = logging.Formatter(
     "(%(asctime)s) %(name)s: "  # prepend message with name and time
     "%(message)s",              # actual message
-    "%I:%M %p"                   # date format
+    "%I:%M %p"                  # date format
 )
 handlr.setFormatter(fmtr)
 
@@ -33,44 +34,78 @@ del handlr, fmtr
 
 
 def get_port(host, port):
-    # negotiation process
+    # open a TCP socket.
     conn = socket.socket(type=socket.SOCK_STREAM)
-
-    # SOCK_STREAM == TCP Socket
     server = (host, port)
 
+    # establish a client connection to the server.
     conn.connect(server)
+
     """I like this idea. Sending a connection-type code.
-    Perhaps 258 to have the server log to this connection."""
+    Perhaps 258 to have the server log to this connection.
+    logging.handlers.SocketHandler. """
     conn.send(b"259")
 
+    """
+    Receive 5 bytes over TCP that should be an integer
+    from 1024 to 65535 referring to the port the file
+    number will be sent over.
+    """
     port = conn.recv(5)
+
+    # close the TCP connection.
     conn.close()
-    return int(tostr(port))
+
+    # return the port used for UDP transfer.
+    return int(port)
 
 
 def send_file(host, port, filename):
-    # file transfer process
+    # open a UDP socket with a timeout of 2 seconds.
     conn = socket.socket(type=socket.SOCK_DGRAM)
-    # SOCK_DGRAM == UDP Socket
+    conn.settimeout(2)
+
     server = (host, port)
+
+    # open the file and send the data over UDP.
     with open(filename, "rb") as file:
         sdata = b"F"
         while sdata[-1:] != b"T":
+            # read the data from the file.
             sdata = file.read(4)
+
+            """
+            Check the length of the data received from
+            the file. If the length of the data is less
+            than 4, the file is assumed to be finished.
+            """
             if len(sdata) < 4:
                 sdata += b"T"
             else:
                 sdata += b"F"
 
+            # send the data over UDP.
             conn.sendto(sdata, server)
             try:
                 ack, addr = conn.recvfrom(4)
-                root_logger.debug(
+                root_logger.info(
                     repr(tostr(ack))
                 )
+            except socket.timeout:
+                # ensure that the file gets closed.
+                root_logger.info("File transfer unsuccessful. Please try again.")
+                break
             except socket.error:
-                root_logger.info("'' - Malformed data? Timeout?")
+                """
+                The socket module has only general 'error' and
+                'timeout' errors. This exception handles the
+                general errors. Timeouts can sometimes throw
+                general errors.
+                """
+                root_logger.info(
+                    "'' - Malformed data? Timeout?"
+                )
+    conn.close()
 
 
 if __name__ == '__main__':
@@ -83,6 +118,7 @@ if __name__ == '__main__':
 
         # TCP negotiation
         new_port = get_port(hostname, int(argv[2]))
+        root_logger.debug("Port is " + str(new_port))
 
         # UDP file transfer
         send_file(hostname, new_port, argv[3])
@@ -91,7 +127,7 @@ if __name__ == '__main__':
             "Usage:   client <hostname> <n_port> <file>\n"
             "Example: client   pluto      8080  file.txt"
         )
-        root_logger.info(e)
+        root_logger.debug(e)
     except OSError as e:
-        root_logger.info("Something went wrong.")
-        root_logger.info(e)
+        root_logger.info("Something went wrong. Please try again.")
+        root_logger.debug(e)
--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import random
 
 import logging
 from sys import stderr, argv, version_info
+from contextlib import closing
 
 
 def tobytes(x):
@@ -20,8 +21,9 @@ def tostr(x):
         return str(x)
 
 
-# logger setup
+# logging setup
 root_logger = logging.getLogger("server")
+# root_logger.setLevel(logging.DEBUG)
 root_logger.setLevel(logging.INFO)
 
 # handler setup
@@ -46,17 +48,37 @@ def select_port(sock, port):
     sock.bind((host, port))
     sock.listen(1)  # become a server socket, max 1 connection.
 
+    # recieve connection from client.
     conn, addr = sock.accept()  # blocking
     root_logger.debug("{} connected.".format(addr))
     data = conn.recv(3)  # assume data is b'259'
     if data == b"259":
         root_logger.debug("Recieved correct message.")
+
+    # generate a random port.
     r_port = random.randint(1024, 65535)
+
+    # check whether port is open.
+    port_taken = True
+    while port_taken:
+        with closing(socket.socket(type=socket.SOCK_STREAM)) as test_sock:
+            # may screw over other people, but nobody
+            # should be testing their code after the
+            # submit date.
+            if test_sock.connect_ex((host, port)) == 0:
+                port_taken = False
+            else:
+                root_logger.debug("Port was taken. Selecting new port.")
+                r_port = random.randint(1024, 65535)
+
+    # log negotiation message.
     root_logger.info(
         "Negotiation has been detected. "
         "Please select your special random port "
         + str(r_port)
     )
+
+    # send port to client.
     conn.send(tobytes(str(r_port)))
     root_logger.debug("Sent negotiation port to {}".format(addr))
     conn.close()
@@ -67,23 +89,33 @@ def receive_file(sock, port):
     # file transfer process
     host = socket.gethostname()
     sock.bind((host, port))
-    sock.settimeout(None)
+    sock.settimeout(2)
 
     with open("output.txt", "wb") as file:
+        finished = False
         while True:
-            data, addr = sock.recvfrom(5)
-            # data is type bytes in the form b'\w{,4}(F|T)'
-            # addr is type Tuple[str, int]
+            try:
+                data, addr = sock.recvfrom(5)
+                # data is type bytes in the form b'\w{,4}(F|T)'
+                # addr is type Tuple[str, int]
 
-            file.write(data[:-1])
-            sock.sendto(tobytes(tostr(data)[:-1].upper()), addr)
+                file.write(data[:-1])
+                sock.sendto(tobytes(tostr(data)[:-1].upper()), addr)
 
-            if data[-1:] == b"T":
-                # At this point the data has been
-                # written to the file, so we can
-                # safely exit the loop.
+                if data[-1:] == b"T":
+                    # At this point the data has been
+                    # written to the file, so we can
+                    # safely exit the loop.
+                    finished = True
+                    break
+            except socket.timeout:
+                # ensure that the file gets closed.
                 break
-        root_logger.debug("File Transfer Complete.")
+
+        if finished:
+            root_logger.debug("File transfer complete.")
+        else:
+            root_logger.info("File transfer interrupted. Please try again.")
 
 
 if __name__ == '__main__':
@@ -91,6 +123,7 @@ if __name__ == '__main__':
         # TCP negotiation
         ssock = socket.socket(type=socket.SOCK_STREAM)
         rport = select_port(ssock, int(argv[1]))
+        root_logger.debug("Port is " + str(rport))
         ssock.close()
 
         # UDP file transfer
@@ -103,7 +136,7 @@ if __name__ == '__main__':
             "Example: server  pluto"
         )
     except OSError as e:
-        root_logger.info("Something went wrong.")
+        root_logger.info("Something went wrong. Please try again.")
         root_logger.debug(e)
     except (SystemExit, SystemError):
         # just make sure that the socket is closed.
